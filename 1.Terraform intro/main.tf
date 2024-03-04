649efb118aa1fe788d741d1640197fc3ebe79c30
jgamezpo <jose.gamez.poza@nttdata.com>
2024-03-04 17:34:01 +0100

    Creación de plan de ejecución con la orden terraform plan -out rg.tfplan

--- 1.Terraform intro/main.tf
+++ 1.Terraform intro/main.tf
@@ -9,8 +9,10 @@ terraform {
 }
 
 provider "azurerm"{
-    alias = "pruebaLocal"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
+    features{}
+}
+
+resource "azurerm_resource_group" "ejemploCursoTerraformIntro" {
+  name     = "gr-curso-udemy-terraform-intro"
+  location = "West Europe"
 }
