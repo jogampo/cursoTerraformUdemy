6d9936fb3632d39f8315fe021176c77128253c64
jgamezpo <jose.gamez.poza@nttdata.com>
2024-03-15 15:03:16 +0100

    Uso de un nuevo módulo az_networking

--- 2. Terraform lab1/main.tf
+++ 2. Terraform lab1/main.tf
@@ -18,18 +18,10 @@ resource "azurerm_resource_group" "rgdemoLabCursoUdemy" {
   }
 
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "demoLab-vnet"
-  location            = azurerm_resource_group.rgdemoLabCursoUdemy.location
-  resource_group_name = azurerm_resource_group.rgdemoLabCursoUdemy.name
-  address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  tags = {
-    environment = "Production"
-  }
+module "vnet"{
+    source = "./modules/az_networking"
+    rg_location = azurerm_resource_group.rgdemoLabCursoUdemy.location
+    rg_name = azurerm_resource_group.rgdemoLabCursoUdemy.name
 }
+
+
---
+++ 2. Terraform lab1/modules/az_networking/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demoLab-vnet"
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
