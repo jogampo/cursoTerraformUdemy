ff87c9eaa311db40e961c41a25b56b42faa77b50
jgamezpo <jose.gamez.poza@nttdata.com>
2024-03-15 14:43:38 +0100

    agregamos los ficheros de variables y de output.

--- 2. Terraform lab1/main.tf
+++ 2. Terraform lab1/main.tf
@@ -12,16 +12,16 @@ provider "azurerm"{
     features{}
 }
 
-resource "azurerm_resource_group" "demoLabCursoUdemy" {
-  name     = "gr-demoLabCursoUdemy"
-  location = "West Europe"
-}
+resource "azurerm_resource_group" "rgdemoLabCursoUdemy" {
+  name     = var.rg_name
+  location = var.rg_location
+  }
 
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "demoLab-vnet"
-  location            = azurerm_resource_group.demoLabCursoUdemy.location
-  resource_group_name = azurerm_resource_group.demoLabCursoUdemy.name
+  location            = azurerm_resource_group.rgdemoLabCursoUdemy.location
+  resource_group_name = azurerm_resource_group.rgdemoLabCursoUdemy.name
   address_space       = ["10.0.0.0/16"]
 
   subnet {
---
+++ 2. Terraform lab1/outputs.tf
@@ -0,0 +1,7 @@
+output "rg_id" {
+  value = azurerm_resource_group.rgdemoLabCursoUdemy.id
+}
+
+output "rg_location"{
+    value = azurerm_resource_group.rgdemoLabCursoUdemy.location
+}
